87fc409af536ba39619ad84b18ec80b0b9986712
Jonathan Sanchez <78042252+jonathan-sanchez@users.noreply.github.com>
2024-12-14 12:51:19 -0600

    Added copyright license and updated packages

---
+++ src/labs/pm/app/Shop.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>
+ */
+
+package labs.pm.app;
+
+/**
+ * {@code Shop} class represents an application that manages Products
+ * @version 4.0
+ * @author $(Jonathan)
+ */
+public class Shop {
+
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
