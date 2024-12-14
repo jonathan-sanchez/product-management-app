d04351cfa0cf6b91d1861c34f4c2417696e9b4f7
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2024-12-14 16:56:44 -0600

    added constructors to Product class and created Rating enum

--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -26,6 +26,18 @@ public class Product {
      * Discount rate is 10%
      */
     public static final BigDecimal DISCOUNT_RATE=BigDecimal.valueOf(0.1);
+    private Rating rating;
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, Rating.NOT_RATED);
+    }
 
     public int getId() {
         return id;
@@ -59,5 +71,9 @@ public class Product {
      */
     public BigDecimal getDiscount() {
         return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public Rating getRating() {
+        return rating;
     }
 }
