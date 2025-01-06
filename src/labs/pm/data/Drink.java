e2ce91df21a072c01104adea6843e2713292e7c2
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2025-01-06 15:19:51 -0600

    completed practice 6-1

--- src/labs/pm/data/Drink.java
+++ src/labs/pm/data/Drink.java
@@ -1,7 +1,13 @@
 package labs.pm.data;
 
+import java.math.BigDecimal;
+
 /**
  * @author $(USER)
  **/
 public class Drink extends Product{
+
+    public Drink(int id, String name, BigDecimal price, Rating rating) {
+        super(id, name, price, rating);
+    }
 }
--- src/labs/pm/data/Food.java
+++ src/labs/pm/data/Food.java
@@ -1,7 +1,21 @@
 package labs.pm.data;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 /**
  * @author $(USER)
  **/
 public class Food extends Product{
+
+    private LocalDate bestBefore;
+
+    public Food(int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore) {
+        super(id, name, price, rating);
+        this.bestBefore = bestBefore;
+    }
+
+    public LocalDate getBestBefore() {
+        return bestBefore;
+    }
 }
