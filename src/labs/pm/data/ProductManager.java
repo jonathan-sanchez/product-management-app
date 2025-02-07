31cd6aef1d5a15d2b0212f319ab5828a25bf41c0
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2025-02-06 19:21:50 -0600

    created the resources.properties file

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -1,7 +1,11 @@
 package labs.pm.data;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * @author $(USER)
@@ -10,6 +14,14 @@ public class ProductManager {
 
     private Product product;
     private Review review;
+    private Locale locale;
+    private ResourceBundle resources;
+    private DateTimeFormatter dateFormat;
+    private NumberFormat moneyFormat;
+
+    public ProductManager(Locale locale) {
+        this.locale = locale;
+    }
 
     public Product createProduct(int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore) {
         product = new Food(id, name, price, rating, bestBefore);
