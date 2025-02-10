2417ca376261f673697f8204aaae8ffe6d9bd08e
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2025-02-10 13:01:57 -0600

    modified logic of the reviewProduct method

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -6,6 +6,7 @@ import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -15,7 +16,7 @@ import java.util.ResourceBundle;
 public class ProductManager {
 
     private Product product;
-    private Review review;
+    private Review[] reviews = new Review[5];
     private Locale locale;
     private ResourceBundle resources;
     private DateTimeFormatter dateFormat;
@@ -39,8 +40,20 @@ public class ProductManager {
     }
 
     public Product reviewProduct(Product product, Rating rating, String comments) {
-        review = new Review(rating, comments);
-        this.product = product.applyRating(rating);
+        if (reviews[reviews.length-1] != null) {
+            reviews = Arrays.copyOf(reviews, reviews.length+5);
+        }
+        int sum = 0, i = 0;
+        boolean reviewed = false;
+        while (i < reviews.length && !reviewed) {
+            if (reviews[i] == null) {
+                reviews[i] = new Review(rating, comments);
+                reviewed = true;
+            }
+            sum += reviews[i].rating().ordinal();
+            i++;
+        }
+        this.product = product.applyRating(Rateable.convert(Math.round((float)sum/i)));
         return this.product;
     }
 
