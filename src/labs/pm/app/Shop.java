5be8c291625e5cbc88874958851ab765e9c0e78c
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2025-09-16 16:14:52 -0500

    Completed chapter 12-2

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -29,14 +29,14 @@ public class Shop {
 
         //P1
         //create product, review it, print report
-        pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
+        pm.parseProduct("D,101,Tea,1.99,0,2021-09-21");
         pm.printProductReport(101);
-        pm.reviewProduct(101, Rating.FOUR_STAR, "Nice hot cup of tea");
-        pm.reviewProduct(101, Rating.TWO_STAR, "Rather weak tea");
-        pm.reviewProduct(101, Rating.FOUR_STAR, "Fine tea");
-        pm.reviewProduct(101, Rating.FOUR_STAR, "Good tea");
-        pm.reviewProduct(101, Rating.FIVE_STAR, "Perfect tea");
-        pm.reviewProduct(101, Rating.THREE_STAR, "Just add some lemon");
+        pm.parseReview("101,4,Nice hot cup of tea");
+        pm.parseReview("101,2,Rather weak tea");
+        pm.parseReview("101,4,Fine tea");
+        pm.parseReview("101,4,Good tea");
+        pm.parseReview("101,5,Perfect tea");
+        pm.parseReview("101,3,Just add some lemon");
         pm.printProductReport(101);
 
         //P2
--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -3,8 +3,10 @@ package labs.pm.data;
 import java.math.BigDecimal;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.format.FormatStyle;
 import java.util.*;
 import java.util.function.Predicate;
@@ -27,6 +29,12 @@ public class ProductManager {
                     "zh-CN", new ResourceFormatter(Locale.CHINA));
 
     private static final Logger logger = Logger.getLogger(ProductManager.class.getName());
+
+    private ResourceBundle config = ResourceBundle.getBundle("labs.pm.data.config");
+
+    private MessageFormat reviewFormat = new MessageFormat(config.getString("review.data.format"));
+
+    private MessageFormat productFormat = new MessageFormat(config.getString("product.data.format"));
 
     private ResourceFormatter formatter;
 
@@ -149,6 +157,36 @@ public class ProductManager {
 //            txt.append('\n');
 //        }
         System.out.println(txt);
+    }
+
+    public void parseReview(String text) {
+        try {
+            Object[] values = reviewFormat.parse(text);
+            reviewProduct(Integer.parseInt((String) values[0]), Rateable.convert(Integer.parseInt((String) values[1])), (String) values[2]);
+        } catch (ParseException | NumberFormatException e) {
+            logger.log(Level.WARNING, "Error parsing review " + text + " " + e.getMessage());
+        }
+    }
+
+    public void parseProduct(String text) {
+        try {
+            Object[] values = productFormat.parse(text);
+            int id = Integer.parseInt((String) values[1]);
+            String name = (String) values[2];
+            BigDecimal price = BigDecimal.valueOf(Double.parseDouble((String) values[3]));
+            Rating rating = Rateable.convert(Integer.parseInt((String) values[4]));
+
+            switch ((String) values[0]) {
+                case "D":
+                    createProduct(id, name, price, rating);
+                    break;
+                case "F":
+                    LocalDate bestBefore = LocalDate.parse((String) values[5]);
+                    createProduct(id, name, price, rating, bestBefore);
+            }
+        } catch (ParseException | NumberFormatException | DateTimeParseException e) {
+            logger.log(Level.WARNING, "Error parsing product " + text + " " + e.getMessage());
+        }
     }
 
     public Map<String, String> getDiscounts() {
